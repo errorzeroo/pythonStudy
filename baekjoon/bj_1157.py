477984aaeb887ebf164b751f08a125b554f06c44
Admin <dlatpwls524@gmail.com>
2022-06-13 18:48:26 +0900

    baekjoon 1157

--- baekjoon/bj_1157.py
+++ baekjoon/bj_1157.py
@@ -4,7 +4,7 @@ word_set = list(word_set)  # set을 다시 list로 바꾼다.
 word_cnt = list()  # count 받을 list를 새로 만든다.
 
 for i in word_set:  # set으로 받은 리스트를 for에 넣어준다.
-    cnt = word.count(i)  # for문에서 돌아가는 문자를 처음받은 문자의 수로 cnt변수로 받아준다.
+    cnt = word.count(i)  # for문에서 돌아가는 문자를 처음받은 문자의 수로 cnt변수로 받아 준다.
     word_cnt.append(cnt)  # 빈 count 리스트에 추가 해준다.
 
 if len(word_cnt) == 1:  # input을 1개만 받았을 때 그대로 프린트 해준다.
