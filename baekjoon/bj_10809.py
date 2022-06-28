ab8cbeba8a8efb01493f2b6719cfd561bfef7247
Admin <dlatpwls524@gmail.com>
2022-06-28 14:39:32 +0900

    baekjoon 10809, 10951

---
+++ baekjoon/bj_10809.py
@@ -0,0 +1,7 @@
+alpha = input()
+word = []
+for i in range(alpha):
+    if alpha in word:
+        print(alpha[i])
+    else:
+        print(-1)
---
+++ baekjoon/bj_10951.py
@@ -0,0 +1,3 @@
+for i in range(5):
+    a, b = map(int, input().split())
+    print(a+b)
