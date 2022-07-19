2b5872757ed8a9cd705f57b781f4e4875e0e2172
Admin <dlatpwls524@gmail.com>
2022-07-19 21:41:27 +0900

    baekjoon 1259, 2750

--- baekjoon/bj_1259.py
+++ baekjoon/bj_1259.py
@@ -1,8 +1,9 @@
-while 1:
+# 팰린드롬수
+while 1:                  # 무한 루프
     pelindlom = input()
-    if pelindlom == '0':
+    if pelindlom == '0':  # '0'이 들어 오면 종료
         break
     elif pelindlom == pelindlom[::-1]:
-        print('yes')
+        print('yes')      # 앞으로 읽어도 뒤로 읽어도 같으면 팰린드롬
     else:
         print('no')
--- baekjoon/bj_2750.py
+++ baekjoon/bj_2750.py
@@ -1,7 +1,8 @@
-N = int(input())
-count = []
-for i in range(N):
+# 수 정렬하기
+N = int(input())         # 인풋 받을 갯수 인풋
+count = []               # count 리스트로 초기화
+for i in range(N):       # 인풋 받은 N만큼 for문 돌기
     i = int(input())
-    count.append(i)
-for j in sorted(count):
+    count.append(i)      # 빈 count 리스트에 인풋 받은 i 넣기
+for j in sorted(count):  # count 정렬 하고 for문 돌려 값 뽑기
     print(j)
