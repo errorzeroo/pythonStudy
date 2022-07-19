1c5657960c42f76053f5c45b33d502293d47e091
Admin <dlatpwls524@gmail.com>
2022-07-19 21:24:35 +0900

    baekjoon 2908

--- baekjoon/bj_2908.py
+++ baekjoon/bj_2908.py
@@ -1,6 +1,6 @@
 # 상수
 num_a, num_b = input().split()
 if num_a[::-1] > num_b[::-1]:    # 문자열 슬라이싱 거꾸로 뽑기
-    print(num_a[::-1], sep=',')  # 거꾸로 큰 수 출력
+    print(num_a[::-1])  # 거꾸로 큰 수 출력
 else:
     print(num_b[::-1])
