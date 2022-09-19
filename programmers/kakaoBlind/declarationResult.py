d5c93d140522ae0965b6995bdb022dbb7083fb7e
errorzeroo <dlatpwls524@gmail.com>
2022-09-20 01:39:25 +0900

    programmers 신고 결과 받기(푸는 중)

---
+++ programmers/kakaoBlind/declarationResult.py
@@ -0,0 +1,12 @@
+def solution(id_list, report, k):
+    answer = [0 for i in range(len(id_list))]
+    set(report)
+    duplicate = [0 for i in range(len(id_list))]
+    for i in report:
+        temp = i[i.find(' ')+1:]
+        for j, idx in enumerate(id_list):
+            if j == temp:
+                duplicate[idx] = +1
+                break
+
+    return answer
